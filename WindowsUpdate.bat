6a8beaf091504f8ab9eb5de2d3507f92be461aee
Infernalblight <129289821+Infernalblight@users.noreply.github.com>
2025-01-05 11:47:33 +0100

    Delete WindowsUpdate.bat

--- WindowsUpdate.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-msg * "Your Windows drivers will be updating. Please wait..."
-timeout /t 10 /nobreak > NUL  :: Wait for 10 seconds
-
-shutdown /r /t 0  :: Restart the PC after 10 seconds
